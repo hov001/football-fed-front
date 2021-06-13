f95026c535b8d0eeb39efbc323d9d356bb08f7be
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-13 14:16:14 +0400

    Feat(gallery section): Added gallery section

--- src/components/homepage/homepage.build.js
+++ src/components/homepage/homepage.build.js
@@ -1,22 +1,13 @@
 import './homepage.css'
 // Header
 import header from '@header/header.js'
-import headerStickySection from '@components/testComps/headerStickySection.js'
 // Home Slider
 import homeSlider from '@homeSlider/homeSlider.js'
 // Main Section
 import mainSection from '@mainSection/mainSection.js'
-// Footer
+// Partners
 import partners from '@components/partners/partner.js'
 // Footer
-import bigNewsBlock from '@components/blocks/bigNewsBlock/bigNewsBlock.js'
-import smallNewsBlock from '@components/blocks/smallNewsBlock/smallNewsBlock.js'
-import table from '@components/tourTable/table.js'
-import tourTableTeam from '@components/tourTableTeam/tourTableTeam.js'
-
-
-
-
 import footer from '@footer/footer.js'
 // // Classes
 // import Slider from '@model/Slider'
--- src/components/homepage/mainSection/mainSection.js
+++ src/components/homepage/mainSection/mainSection.js
@@ -4,11 +4,13 @@ import './mainSection.responsive.css'
 // National calendar
 import nationalCalendar from '@nationalCalendar/nationalCalendar.js'
 import generalNews from '@generalNews/generalNews.js'
+import gallerySection from '@gallerySection/gallerySection.js'
 
 const mainSection = `
   <section class="main-section">
     ${nationalCalendar}
     ${generalNews}
+    ${gallerySection}
   </section>
 `
 
